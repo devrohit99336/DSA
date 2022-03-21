b6e639ad87c326b517d962dbf6596fe2b6038a0b
devrohit99336 <official26rohit@gmail.com>
2022-03-21 08:15:10 +0530

    init

---
+++ DSA in C&C++/programms/basics/primeNo.c
@@ -0,0 +1,7 @@
+/*** 
+ * Prime Numbers (अभाज्य संख्या) वो whole numbers होते हैं जो सिर्फ number 1 और अपने अलावा किसी और numbers से divisible नहीं होते हैं.
+ * 
+ * prime numbers उन numbers को कहा जाता है जिनमें अगर आप number `1 और खुद उस number के अलावा किसी और numbers से division करते हैं तो remainder (शेषफल ) 0 नहीं आता है.
+ * 
+ * वो numbers जो number 1 और अपने अलावा और भी numbers से divisable होते हैं उन्हें हम Composite Numbers कहते हैं लेकिन number 1 ना तो prime number है और ना ही composite number.
+*/
